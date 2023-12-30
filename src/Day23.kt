6463901cdaa303a1c672e6f26d55e753dd94cce0
Tomasz Linkowski <t.linkowski@gmail.com>
2023-12-30 20:38:47 +0100

    Day23 (refactored)

--- src/Day23.kt
+++ src/Day23.kt
@@ -1,22 +1,34 @@
+import kotlin.math.max
+
 fun main() {
     val day = "Day23"
 
     // MODEL
-    data class Forest(
+    data class ForestMap(
         val gridRange: GridRange, val start: Point, val end: Point,
-        val trees: Set<Point>, val slopes: Map<Point, Dir>,
-        val junctionPoints: Set<Point>
+        val trees: Set<Point>, val slopes: Map<Point, Dir>, val junctions: Set<Point>
     )
 
-    data class Graph(
-        val start: Point,
-        val end: Point,
-        val neighbors: Map<Point, Set<Point>>,
-        val lengths: Map<Set<Point>, Int>
+    data class Position(val sourceDir: Dir, val point: Point)
+
+    val pointComparator = compareBy<Point> { it.x }.thenBy { it.y }
+
+    data class Edge(val a: Point, val b: Point) {
+        init {
+            check(pointComparator.compare(a, b) < 0)
+        }
+    }
+
+    data class ForestGraph(
+        val start: Point, val end: Point,
+        val neighbors: Map<Point, List<Point>>,
+        val paths: Map<Edge, List<Point>>
     )
+
+    data class Hike(val currentNode: Point, val visitedNodes: Set<Point>, val distance: Int)
 
     // PARSE
-    fun List<String>.parseForest(): Forest {
+    fun List<String>.parseForestMap(): ForestMap {
         val gridRange = toGridRange()
         val pointMap = toPointMap()
         val trees = pointMap.filterValues { it == '#' }.keys
@@ -31,170 +43,149 @@ fun main() {
         }
         val start = pointMap.firstNotNullOf { (p, c) -> if (p.y == 0 && c == '.') p else null }
         val end = pointMap.firstNotNullOf { (p, c) -> if (p.y == size - 1 && c == '.') p else null }
-        val junctions = pointMap.keys
-            .filter { p -> Dir.entries.map { d -> p.move(d) }.count { mp -> mp in gridRange && mp !in trees } > 2 }
+
+        fun isPassable(point: Point) = point in gridRange && point !in trees
+        fun neighborCount(point: Point) = Dir.entries.map(point::move).count(::isPassable)
+        val junctions = pointMap.filterValues { it == '.' }.keys
+            .filter { point -> neighborCount(point) > 2 }
             .toSet()
-        return Forest(gridRange, start, end, trees, slopes, junctions)
+
+        return ForestMap(gridRange, start, end, trees, slopes, junctions)
     }
 
     // SOLVE
-    fun Forest.neighbor(source: Point, dir: Dir): Pair<Point, Int>? {
-        check(source in junctionPoints)
-        var lastDir = dir
-        var p = source.move(dir)
-        var len = 1
-        while (p != start && p != end && p !in junctionPoints) {
-            val pair = Dir.entries
-                .filter { it != lastDir.opposite() }
-                .map { it to p.move(it) }
-                .singleOrNull { it.second in gridRange && it.second !in trees }
+    fun ForestMap.isPassable(point: Point) = point in gridRange && point !in trees
+
+    fun ForestMap.isGraphNode(point: Point) = point == start || point == end || point in junctions
+
+    fun ForestMap.initialDirections(point: Point) = if (point in slopes) listOf(slopes[point]!!) else Dir.entries
+
+    fun Point.toPositionAlong(dir: Dir) = Position(dir, move(dir))
+
+    fun Position.tryMoveAlong(dir: Dir) = if (dir != sourceDir.opposite()) point.toPositionAlong(dir) else null
+
+    fun ForestMap.findPathAlongDirection(source: Point, sourceDir: Dir): List<Point>? {
+        check(isGraphNode(source))
+        var position = source.toPositionAlong(sourceDir)
+        val path = mutableListOf(source, position.point)
+
+        while (!isGraphNode(position.point)) {
+            position = initialDirections(position.point)
+                .mapNotNull { dir -> position.tryMoveAlong(dir) }
+                .singleOrNull { isPassable(it.point) }
                 ?: return null
-            lastDir = pair.first
-            p = pair.second
-            len++
+            path += position.point
         }
 
-        return p to len
+        return path
     }
 
-    fun Forest.canWalk(p: Point) = p in gridRange && p !in trees
+    fun edge(a: Point, b: Point) = if (pointComparator.compare(a, b) < 0) Edge(a, b) else Edge(b, a)
 
-    fun Forest.toGraph(): Graph {
-        val neighbors = mutableMapOf<Point, MutableSet<Point>>()
-        val lengths = mutableMapOf<Set<Point>, Int>()
+    fun ForestMap.toGraph(directed: Boolean): ForestGraph {
+        val neighbors = mutableMapOf<Point, MutableList<Point>>()
+        val paths = mutableMapOf<Edge, List<Point>>()
 
-        for (j in junctionPoints) {
-            Dir.entries.asSequence()
-                .filter { dir -> canWalk(j.move(dir)) }
-                .mapNotNull { dir -> neighbor(j, dir) }
-                .forEach { (np, len) ->
-                    neighbors.at(j) += np
-                    neighbors.at(np) += j
-                    lengths[setOf(j, np)] = len
-                }
-        }
-        println("Stats: neighbors.size=${neighbors.size}, lengths.size=${lengths.size}, branches=${neighbors.values.sumOf {it.size} / 2}")
-        return Graph(start, end, neighbors, lengths)
-    }
-
-    data class GHike(val cur: Point, val visited: Set<Point>, val length: Int)
-
-    fun Graph.nextSteps(hike: GHike): List<GHike> {
-        val p = hike.cur
-        check(p != end)
-        return neighbors[p]!!
-            .filter { it !in hike.visited }
-            .map { GHike(it, hike.visited + it, hike.length + lengths[setOf(p, it)]!!)}
-    }
-
-    fun Forest.toNiceString(graph: Graph): String {
-        return gridRange.toNiceString { p ->
-            when (p) {
-                in trees -> '#'
-                in graph.neighbors -> graph.neighbors[p]!!.size.toString().last()
-//                in graph.neighbors -> {
-//                    val neighbor = graph.neighbors[p]!!.first()
-//                    graph.lengths[setOf(p, neighbor)]!!.toString().last()
-//                }
-                else -> '.'
-            }
-        }
-        // Dir.entries
-        //                    .map { p.move(it) }
-        //                    .mapNotNull { graph.neighbors[it]?. }
-        //
-        //
-    }
-
-    fun Forest.toNiceString(hike: GHike): String {
-        return gridRange.toNiceString { p ->
-            when (p) {
-                in trees -> '#'
-                in hike.visited -> 'x'
-//                in slopes -> slopes[p]!!.symbol
-//                in junctionPoints -> 'x'
-                else -> '.'
-            }
-        }
-    }
-
-    fun Graph.longestHikeMap(): GHike {
-        val startHike = GHike(start, setOf(start), 0)
-        val endHikes = mutableListOf<GHike>()
-
-        var hikesInProgress = mapOf(start to listOf(startHike))
-
-        while (hikesInProgress.isNotEmpty()) {
-//            if (hikes.size < 10) println(hikes)
-            println("End: ${endHikes.size}, in progress: ${hikesInProgress.values.sumOf { it.size}}")
-            val nextHikesInProgress = mutableMapOf<Point, MutableList<GHike>>()
-            for (hh in hikesInProgress.values) {
-                for (h in hh) {
-                    nextSteps(h).forEach { nh ->
-                        if (nh.cur == end) endHikes += nh else nextHikesInProgress.at(nh.cur) += nh
-                    }
-                }
-            }
-            hikesInProgress = nextHikesInProgress
-                .mapValues { (_, v) ->
-                    val maxLength = v.maxOf { it.length }
-                    v.filter { it.length >= maxLength - 500 }
+        val nodes = junctions + start + end
+        for (node in nodes) {
+            Dir.entries
+                .filter { dir -> isPassable(node.move(dir)) }
+                .mapNotNull { dir -> findPathAlongDirection(node, dir) }
+                .forEach { path ->
+                    val source = path.first()
+                    val target = path.last()
+                    neighbors.at(source) += target
+                    if (!directed) neighbors.at(target) += source
+                    paths[edge(source, target)] = path
                 }
         }
 
-        val longest = endHikes.maxBy { it.length }
-//        println(toNiceString(longest))
-        return longest
+        println("Nodes: ${nodes.size}, directed edges: ${neighbors.values.sumOf { it.size } / 2}, total distance: ${paths.values.sumOf { it.size - 1 }}")
+        return ForestGraph(start, end, neighbors, paths)
     }
 
-    fun Graph.longestHike(): GHike {
-        val startHike = GHike(start, setOf(start), 0)
-        val endHikes = mutableListOf<GHike>()
+    fun ForestGraph.neighbors(point: Point) = neighbors[point]!!
 
-        var hikesInProgress = listOf(startHike)
-        while (hikesInProgress.isNotEmpty()) {
-//            if (hikes.size < 10) println(hikes)
-            println("End: ${endHikes.size}, in progress: ${hikesInProgress.size}")
-            val nextHikesInProgress = ArrayList<GHike>(hikesInProgress.size * 2)
-            for (h in hikesInProgress) {
-                nextSteps(h).forEach { nh ->
-                    if (nh.cur == end) endHikes += nh else nextHikesInProgress += nh
+    fun ForestGraph.pathBetween(a: Point, b: Point) = paths[edge(a, b)]!!
+
+    fun ForestGraph.distanceBetween(a: Point, b: Point): Int = pathBetween(a, b).size - 1
+
+    fun ForestGraph.tryHikeTo(hike: Hike, next: Point): Hike? = if (next !in hike.visitedNodes)
+        Hike(next, hike.visitedNodes + next, hike.distance + distanceBetween(hike.currentNode, next))
+    else null
+
+    fun ForestGraph.tryHikeToNeighbors(hike: Hike): List<Hike> = neighbors(hike.currentNode)
+        .mapNotNull { neighbor -> tryHikeTo(hike, neighbor) }
+
+    val bestDistanceRatio = 0.90
+
+    fun ForestGraph.longestHike(): Hike {
+        val seed = Hike(start, setOf(start), 0)
+        var inProgress = mapOf(start to listOf(seed))
+        var result = seed
+        var maxHikesInProgress = 0 // debug info
+
+        while (inProgress.isNotEmpty()) { // BFS
+            maxHikesInProgress = max(maxHikesInProgress, inProgress.values.sumOf { it.size })
+
+            val nextStepHikes = inProgress.values.asSequence()
+                .flatMap { hikesToPoint ->
+                    // keep only best hikes to given point
+                    val minDistance = bestDistanceRatio * hikesToPoint.maxOf { it.distance }
+                    hikesToPoint.asSequence().filter { it.distance >= minDistance }
                 }
-            }
-            hikesInProgress = nextHikesInProgress
+                .flatMap { hike -> tryHikeToNeighbors(hike) }
 
-//            val result = hikesInProgress.flatMap { nextSteps(it) }
-//            hikesInProgress = result.filter { it.cur != end }
-//            endHikes += result.filter { it.cur == end }
+            inProgress = nextStepHikes.filter { it.currentNode != end }.groupBy { it.currentNode }
+            result = nextStepHikes.filter { it.currentNode == end }.plus(result).maxBy { it.distance }
         }
+        println("Max hikes in progress during BFS: $maxHikesInProgress")
 
-        val longest = endHikes.maxBy { it.length }
-//        println(toNiceString(longest))
-        return longest
+        return result
     }
 
-    fun part2(input: List<String>): Int {
-        val forest = input.parseForest()
-        val graph = forest.toGraph()
-        println()
-        println(forest.toNiceString(graph))
-        println()
-        val longestHike = graph.longestHikeMap()
+    fun Hike.allVisitedPoints(forestGraph: ForestGraph) =
+        visitedNodes.zipWithNext(forestGraph::pathBetween).flatten().toSet()
 
-        return longestHike.length
+    fun ForestMap.toNiceString(visitedPoints: Set<Point>) = gridRange.toNiceString { p ->
+        when (p) {
+            in visitedPoints -> 'O'
+            start -> 'S'
+            end -> 'E'
+            in trees -> '#'
+            in slopes -> slopes[p]!!.symbol
+            in junctions -> '+'
+            else -> '.'
+        }
     }
+
+    fun Hike.toNiceString(forestMap: ForestMap, forestGraph: ForestGraph) =
+        forestMap.toNiceString(allVisitedPoints(forestGraph))
+
+    fun part(input: List<String>, directed: Boolean, print: Boolean): Int {
+        val forestMap = input.parseForestMap()
+        val forestGraph = forestMap.toGraph(directed)
+        val longestHike = forestGraph.longestHike()
+        if (print) println(longestHike.toNiceString(forestMap, forestGraph))
+
+        return longestHike.distance
+    }
+
+    fun part1(input: List<String>, print: Boolean = false): Int = part(input, directed = true, print)
+
+    fun part2(input: List<String>, print: Boolean = false): Int = part(input, directed = false, print)
 
     // TESTS
-//    val test1 = part1(readInput("$day/test"))
-//    94.let { check(test1 == it) { "Test 1: is $test1, should be $it" } }
+    val test1 = part1(readInput("$day/test"), print = true)
+    94.let { check(test1 == it) { "Test 1: is $test1, should be $it" } }
 
-    val test2 = part2(readInput("$day/test"))
+    val test2 = part2(readInput("$day/test"), print = true)
     154.let { check(test2 == it) { "Test 2: is $test2, should be $it" } }
 
     // RESULTS
     val input = readInput("$day/input")
-//    val part1 = part1(input)
-//    2086.let { println("Part 1: $part1" + if (part1 == it) "" else " (should be $it?)") }
+    val part1 = part1(input)
+    2086.let { println("Part 1: $part1" + if (part1 == it) "" else " (should be $it?)") }
     val part2 = part2(input)
-    println("Part 2: $part2") // 4794 too low, 5654 too low
+    println("Part 2: $part2")
 }
